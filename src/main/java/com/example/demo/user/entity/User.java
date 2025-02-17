ea4f7da6d6a706101723f6eb2313e1d0c96b3fef
jiminnimij <010709min@ehwain.net>
2025-02-17 12:09:33 +0900

    fix: user entity 생성

---
+++ src/main/java/com/example/demo/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.user.entity;
+
+import com.example.demo.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cake_category")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String profileImage;
+
+    @Column(nullable = false)
+    private String email;
+}
