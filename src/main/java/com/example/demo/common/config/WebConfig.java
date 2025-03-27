abfb6ef5b575ef7347cf974c366b38225484d698
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-03-26 18:32:00 -0700

    Update WebConfig.java

--- src/main/java/com/example/demo/common/config/WebConfig.java
+++ src/main/java/com/example/demo/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Next.js 도메인
+                .allowedOrigins("http://localhost:3000", "http://localhost:3000/", "http://13.209.207.149:8080") // Next.js 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
