2096ad834d87c0b2533e610408bb32460e15a5fc
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-06-16 17:02:10 +0900

    Update ReviewRequestDto.java

--- src/main/java/com/example/demo/review/dto/ReviewRequestDto.java
+++ src/main/java/com/example/demo/review/dto/ReviewRequestDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class ReviewRequestDto {
     private String exhibitionId;
-    private String exhitibionTitle;
+    private String exhibitionTitle;
     private String exhibitionImageURL;
     private String place;
     private String during;
